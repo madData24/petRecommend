e8136d6381d822ad01b4074a6d3bb49118d26858
Freddy <wdragj@gmail.com>
2024-02-25 11:33:15 -0600

    final

--- app/components/Content.tsx
+++ app/components/Content.tsx
@@ -1,11 +1,11 @@
-import { Typography, Button, Container, Card, CardContent, Grid } from "@mui/material";
+import { Typography, Button, Container, Card, CardContent, Grid, LinearProgress } from "@mui/material";
 import { SubmitButton } from "./SubmitButton";
 import { useState, FormEvent } from "react";
 
 const Content = () => {
     const [inputValue, setInputValue] = useState<string>("");
     const [breedNames, setBreedNames] = useState<string[]>([]);
-    const [result, setResult] = useState(null);
+    const [loading, setLoading] = useState<boolean>(false); // State for loading status
 
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
         setInputValue(e.target.value);
@@ -14,6 +14,7 @@ const Content = () => {
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault();
         console.log("User word:", inputValue);
+        setLoading(true); // Set loading to true when submitting form
         try {
             const response = await fetch("https://wdragj.pythonanywhere.com/api/breedRecommend", {
                 method: "POST",
@@ -31,63 +32,10 @@ const Content = () => {
             const breeds = data.data.breed;
             console.log("Breeds:", breeds);
             setBreedNames(breeds); // Set the breed names
-
-            // Check if data has the 'breed' property
-            // if (breeds) {
-            //     try {
-            //         const tokenResponse = await fetch("https://api.petfinder.com/v2/oauth2/token", {
-            //             method: "POST",
-            //             headers: {
-            //                 "Content-Type": "application/json",
-            //             },
-            //             body: JSON.stringify({
-            //                 grant_type: "client_credentials",
-            //                 client_id: "RYkL8o0DxVBE98vMdkRvVCEGTw7SkgIAXhNsISaahLGML34szc",
-            //                 client_secret: "JXf2XvJnSxUnunToKxAQWfJVZo9qwrF0i1wh555r",
-            //             }),
-            //         });
-
-            //         if (!tokenResponse.ok) {
-            //             throw new Error("Failed to obtain token");
-            //         }
-
-            //         const { access_token } = await tokenResponse.json();
-
-            //         // Convert array of breed names to a single comma-separated string
-            //         const breedNames = breeds.join(", ").trim();
-
-            //         console.log("Breed names: ", breedNames);
-
-            //         // Construct URL with query parameters
-            //         const url = new URL("https://api.petfinder.com/v2/animals");
-            //         url.searchParams.append("breed", breedNames);
-
-            //         const dogsResponse = await fetch(url.toString(), {
-            //             method: "GET",
-            //             headers: {
-            //                 Authorization: `Bearer ${access_token}`,
-            //             },
-            //         });
-
-            //         if (!dogsResponse.ok) {
-            //             throw new Error("Failed to fetch dogs");
-            //         }
-
-            //         const { animals, pagination } = await dogsResponse.json();
-            //         const { count_per_page, total_count, current_page, total_pages, _links } = pagination;
-            //         const { previous, next } = _links;
-            //         console.log("Fetched dogs:", animals);
-            //         console.log("Pagination:", pagination);
-            //         setResult(animals); // Set the dogs data
-            //     } catch (error) {
-            //         console.error("Error:", error);
-            //     }
-            // } else {
-            //     console.error("Error: Invalid data format or missing 'breed' property");
-            // }
         } catch (error) {
             console.error("Error fetching data: ", error);
-            setResult(null);
+        } finally {
+            setLoading(false); // Set loading to false after receiving response or error
         }
     };
 
@@ -106,7 +54,7 @@ const Content = () => {
                 <form
                     action={"/findDog"}
                     className="bg-black rounded-xl shadow-lg h-fit flex flex-row px-1 items-center w-full"
-                    style={{ height: "60px", marginBottom: "120px" }}
+                    style={{ height: "60px", marginBottom: "20px" }}
                     onSubmit={handleSubmit}
                 >
                     <input
@@ -120,8 +68,9 @@ const Content = () => {
                     <input aria-hidden type="text" name="token" value={"token"} className="hidden" readOnly />
                     <SubmitButton />
                 </form>
+                {loading && <LinearProgress sx={{ height: 10 }} />}
             </Container>
-            <Grid container spacing={2} justifyContent="center">
+            <Grid container spacing={2} justifyContent="center" style={{ marginTop: "120px" }}>
                 {breedNames.map((name, index) => (
                     <Grid
                         item
