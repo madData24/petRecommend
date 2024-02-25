2e6adbbdf571b5f6eafd7e4169d53469794c2e2f
Freddy <wdragj@gmail.com>
2024-02-24 22:59:04 -0600

    init

---
+++ pages/index.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+    return (
+        <>
+            <h1>Hello, world!</h1>
+            <p>This is a simple example of a Next.js app with TypeScript and Tailwind CSS.</p>
+        </>
+    );
+}
